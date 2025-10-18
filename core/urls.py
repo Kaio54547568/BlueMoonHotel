9a2ba259b9f4c036d6ffcfca9b9695f062e4ad2e
Lương Văn Hưng <Hung.lv235740@sis.hust.edu.vn>
2025-10-18 21:54:33 +0700

    fontend w2

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='rooms_list'),
+    path('', views.home, name='home'),
+    path('accountmanage', views.accountmanage, name='accountmanage'),
+    path('demomanage', views.demomanage, name='demomanage'),
+    path('test/', views.test, name='test'),
+    path('profile/', views.profile, name='profile'),
     
+    path('hrmanage/', views.hrmanage, name='hrmanage'),
+    path('hredit/', views.hredit, name='hredit'),
 ]
